db79e439131562a0556185d04ac3caf8edcad088
Christine McCabe <christine.mccabe@atu.ie>
2024-04-25 10:41:31 +0100

    Commit with Cars Practical

---
+++ Practical 7_mtcars.R
@@ -0,0 +1,25 @@
+# MTCARS Lab Practical 7 -----------------------------------------
+# This practical session is based on the mtcars data which was 
+# extracted from the 1974 Motor Trend US magazine and comprises 
+# fuel consumption and 10 aspects of automobile design and performance 
+# for 32 cars (1973--74 models). 
+# The variables of the cars data frame are as follows: 
+# mpg  Miles/(US) gallon 
+# cyl  Number of cylinders 
+# disp  Displacement (cu.in.) 
+# hp  Gross horsepower 
+# drat  Rear axle ratio 
+# wt  Weight (1000 lbs) 
+# qsec  1/4 mile time 
+# vs  Engine (0 = V-shaped, 1 = straight) 
+# am  Transmission (0 = automatic, 1 = manual) 
+# gear  Number of forward gears  
+
+# Question 1
+# We would like to examine whether there is a link between the 
+# weight of cars and their fuel efficiency as measured by MPG. 
+# Devise a hypothesis test that we use for this analysis. 
+# Describe the variables you will use for the analysis. 
+# What type of variables are they? 
+# Do they need to be converted? 
+# If they do, convert them to the relevant data types in R. 
