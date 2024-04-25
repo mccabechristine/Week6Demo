3a944a5ac895129b68b2b87fe801e89582dd4c85
Christine McCabe <christine.mccabe@atu.ie>
2024-04-25 12:05:36 +0100

    Final Commit

--- Week 6 Demo_Beavers.R
+++ Week 6 Demo_Beavers.R
@@ -32,7 +32,7 @@ str(beavers_data)
 ## What type of variables are they? Do they need to be converted? 
 ## If they do, convert them to the relevant data types in R. ----#
 
-# labels starts with what is assigned to the lawer value first
+# labels starts with what is assigned to the lower value first
 # eg 0 = no, 1 = yes
 beavers_data$activ <- factor(beavers_data$activ,
                              labels = c("no", "yes"))
@@ -85,7 +85,49 @@ with(beavers_data, {
 
 with(beavers_data, {
   qqnorm(temp[activ == "no"],
+         main = "Beavers inactivewindows(16, 10)
+ggplot(beavers_data, aes(x=temp))+geom_histogram()+theme_bw()
+
+ggplot(beavers_data, aes(x=temp))+
+        geom_histogram(breaks=seq(36, 38, .2))+
+        theme_bw()+
+        labs(x = "temp", y = "Activity")+
+        scale_y_continuous(breaks=seq(0,60,5))
+
+library("lattice")
+windows(20,10)
+attach(beavers_data)
+histogram(~temp | activ,
+          data = beavers_data,
+          main = "Distribution of beaver activity data",
+          xlab = "Temperature (degrees)",
+          ylab = "Activity %")
+detach(beavers_data)
+
+attach(beavers_data)
+windows(15,10)
+qqnorm(temp)
+# this line represents normal distribution
+qqline(temp, col = "red")
+
+opar <- par(no.readonly = TRUE)
+windows(20,10)
+# arrange plots in 1 rows and 2 column
+par(mfrow = c(1, 2))
+
+with(beavers_data, {
+  qqnorm(temp[activ == "yes"],
+         main = "Beavers active data")
+  qqline(temp[activ == "yes"])
+})
+
+with(beavers_data, {
+  qqnorm(temp[activ == "no"],
          main = "Beavers inactive data")
+  qqline(temp[activ == "no"])
+})
+
+par(opar) data")
   qqline(temp[activ == "no"])
 })
 
